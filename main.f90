14ce4b490e9378f8b88bd3369c1f10b02406c017
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-04-26 19:37:38 -0300

    Game loop & EMPTY const
    * Added game loop
    * Added winner & winning conditions
    * Added input checks
    * Added player 1 & 2
    * Added const EMPTY var yay

--- main.f90
+++ main.f90
@@ -4,12 +4,60 @@ program main
     character :: board(3, 3)
     character :: player
     integer :: turn
+    integer :: row
+    integer :: column
+    integer :: winner
+    logical :: valid
+    character, parameter :: EMPTY = '.'
 
     call initBoard(board)
     player = "X"
 
     do turn = 1, 9
         call printBoard(board)
+        
+        valid = .false.
+
+        do while (.not. valid)
+            write(*, "(A)", advance = "no") "Enter row and column (example: '1 3'): "
+            read(*, *, iostat = winner) row, column
+
+            if (winner /= 0) then
+                print *, "Invalid input."
+                cycle
+            end if
+
+            if (row < 1 .or. row > 3 .or. column < 1 .or. column > 3) then
+                print *, "Out of range. Rows/columns must be 1 to 3."
+            else if (board(row, column) /= EMPTY) then
+                print *, "This spot is already taken! Try again."
+            else
+                valid = .true.
+            end if
+        end do
+    
+        board(row, column) = player
+        winner = checkWinner(board)
+    
+        if (winner /= 0) then
+            call printBoard(board)
+
+            print *, "Player ", player, " wins! "
+            stop
+        end if
+    
+        if (turn == 9) then
+            call printBoard(board)
+
+            print *, "It's a draw!"
+            stop
+        end if
+    
+        if (player == 'X') then
+            player = 'O'
+        else
+            player = 'X'
+        end if
     end do
 
 contains
@@ -17,7 +65,7 @@ contains
     subroutine initBoard(b)
         character, intent(out) :: b(3, 3)
 
-        b = '.'
+        b = EMPTY
     end subroutine
 
     subroutine printBoard(b) ! Prints the board in a pretty format
@@ -31,4 +79,37 @@ contains
 
         print *, " "
     end subroutine
+
+    integer function checkWinner(b) ! Check if a player has won in a given board. 
+        character, intent(in) :: b(3,3)
+        integer :: idx
+
+        checkWinner = 0
+    
+        ! Rows and columns
+        do idx = 1, 3
+            if (b(idx, 1) /= EMPTY .and. b(idx, 1) == b(idx, 2) .and. b(idx, 2) == b(idx, 3)) then
+                checkWinner = 1
+                return
+            end if
+
+            if (b(1, idx) /= EMPTY .and. b(1, idx) == b(2, idx) .and. b(2, idx) == b(3, idx)) then
+                checkWinner = 1
+                return
+            end if
+        end do
+    
+        ! Diagonals (top left -> bottom right)
+        if (b(1, 1) /= EMPTY .and. b(1, 1) == b(2, 2) .and. b(2, 2) == b(3, 3)) then
+            checkWinner = 1
+            return
+        end if
+
+        ! Diagonals (top right -> bottom left)
+        if (b(1, 3) /= EMPTY .and. b(1, 3) == b(2, 2) .and. b(2, 2) == b(3, 1)) then
+            checkWinner = 1
+            return
+        end if
+    end function
+
 end program main
